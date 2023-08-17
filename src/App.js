ae7172ae4ccb888e2ca781d51342e49169195d01
Andreas Kobal <kobalandrey@gmail.com>
2023-08-17 02:12:39 +0100

    add mobile support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 import MintNFT from './MintNFT';
+import logo from './assets/logo.png';
+
 
 function App() {
+  const isMobile = window.innerWidth <= 768; // You can adjust the breakpoint if needed
+
   return (
     <div className="App">
+      {isMobile ? (
+        <div className="MobileOverlay">
+          <img src={logo} alt="logo" style={{ width: '150px' }} /> 
+          <h1 style={{ textAlign: 'center', fontSize: '24px', lineHeight: '28px', margin: '16px 0'}}>Mint Your AI NFT Avatar</h1>
+
+          <h4>Enjoy the app on mobile! For a seamless experience, please install Metamask</h4>
+          <a href="https://metamask.app.link/dapp/simple-nft-minter-xt5p.vercel.app/"><button>Open in Metamask</button></a>
+        </div>
+      ) : null}
       <MintNFT />
     </div>
   );
