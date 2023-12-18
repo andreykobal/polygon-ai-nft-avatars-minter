eac4857c59c78e3287e6137beda17c8334b3351d
Andrew Kobal <kobalandrey@gmail.com>
2023-12-18 21:28:11 +0000

    Update balances.js

--- ai-nft-implementation/scripts/balances.js
+++ ai-nft-implementation/scripts/balances.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 
 async function main() {
-  const GameItem = await hre.ethers.getContractFactory("GameItems");
+  const GameItem = await hre.ethers.getContractFactory("AISFT");
   const gameItem = await GameItem.attach('0x643D5cf6fdd9Faa3825e194e925D07E290E993D2');
 
   const account = "0x204f9781DDcafB4a844fd12250dB15183C67cACB";
